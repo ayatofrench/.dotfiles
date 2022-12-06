d15d150ec5dd4131d5891f8c28061ae4e1d0c08a
Ayato French <ayato.french@amnhealthcare.com>
2022-12-05 21:29:22 -0600

    added lsp support for ruby

--- nvim/lua/secretninjaman/mason.lua
+++ nvim/lua/secretninjaman/mason.lua
@@ -1,3 +1,48 @@
-require("mason").setup()
-require("mason-lspconfig").setup()
+local mason = require("mason")
+local mason_lsp = require("mason-lspconfig")
+local lspconfig = require("lspconfig")
+local is_mac = vim.fn.has("macunix") == 1
 
+local lsp = {}
+lsp.servers = {
+    "bashls",
+    "cssls",
+    "dockerls",
+    "html",
+    "jsonls",
+    "rust_analyzer",
+    "sumneko_lua",
+    "tailwindcss",
+    "tsserver",
+    "yamlls",
+}
+---
+mason.setup()
+
+mason_lsp.setup({
+    ensure_installed = lsp.servers,
+    automatic_installation = true,
+})
+
+lspconfig.sumneko_lua.setup {}
+
+if is_mac then
+    lspconfig.solargraph.setup {
+        settings = {
+            solargraph = {
+                commandPath = '/Users/ayato.french/.rvm/gems/ruby-2.5.3/bin/solargraph',
+                diagnostics = true,
+                completion = true
+            }
+        },
+        root_dir = lspconfig.util.root_pattern("Gemfile", ".git"),
+    }
+end
+-- mason_lsp.setup_handlers({
+--   function(server_name)
+--     require("lspconfig")[server_name].setup({
+--       capabilities = capabilities,
+--       on_attach = om.lsp.on_attach,
+--     })
+--   end,
+-- })
--- nvim/lua/secretninjaman/telescope.lua
+++ nvim/lua/secretninjaman/telescope.lua
@@ -6,39 +6,37 @@ local conf = require("telescope.config").values
 local actions = require("telescope.actions")
 
 require("telescope").setup({
-	defaults = {
-		file_sorter = require("telescope.sorters").get_fzy_sorter,
+    defaults = {
+        prompt_prefix = " >",
+        color_devicons = true,
 
+        file_previewer = require("telescope.previewers").vim_buffer_cat.new,
+        grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
+        qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
 
-		prompt_prefix = " >",
+        mappings = {
 
-		color_devicons = true,
+            i = {
 
+                ["<C-x>"] = false,
 
-
-		file_previewer = require("telescope.previewers").vim_buffer_cat.new,
-
-		grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
-
-		qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
-
-
-
-		mappings = {
-
-			i = {
-
-				["<C-x>"] = false,
-
-				["<C-q>"] = actions.send_to_qflist,
+                ["<C-q>"] = actions.send_to_qflist,
 
                 ["<CR>"] = actions.select_default,
 
-			},
+            },
 
-		},
-
-	},
+        },
+        extensions = {
+            fzf = {
+                fuzzy = true, -- false will only do exact matching
+                override_generic_sorter = true, -- override the generic sorter
+                override_file_sorter = true, -- override the file sorter
+                case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+                -- the default case_mode is "smart_case"
+            }
+        }
+    },
 
     --[[
 
@@ -60,10 +58,10 @@ require("telescope").setup({
 
 
 
-require("telescope").load_extension("git_worktree")
+-- require("telescope").load_extension("git_worktree")
 
 -- require("telescope").load_extension("fzy_native")
-
+require('telescope').load_extension('fzf')
 
 
 local M = {}
@@ -72,35 +70,33 @@ local M = {}
 
 function M.reload_modules()
 
-	-- Because TJ gave it to me.  Makes me happpy.  Put it next to his other
+    -- Because TJ gave it to me.  Makes me happpy.  Put it next to his other
 
-	-- awesome things.
+    -- awesome things.
 
-	local lua_dirs = vim.fn.glob("./lua/*", 0, 1)
+    local lua_dirs = vim.fn.glob("./lua/*", 0, 1)
 
-	for _, dir in ipairs(lua_dirs) do
+    for _, dir in ipairs(lua_dirs) do
 
-		dir = string.gsub(dir, "./lua/", "")
+        dir = string.gsub(dir, "./lua/", "")
 
-		require("plenary.reload").reload_module(dir)
+        require("plenary.reload").reload_module(dir)
 
-	end
+    end
 
 end
 
-
-
 M.search_dotfiles = function()
 
-	require("telescope.builtin").find_files({
+    require("telescope.builtin").find_files({
 
-		prompt_title = "< VimRC >",
+        prompt_title = "< VimRC >",
 
-		cwd = vim.env.DOTFILES,
+        cwd = vim.env.DOTFILES,
 
-		hidden = true,
+        hidden = true,
 
-	})
+    })
 
 end
 
@@ -110,83 +106,77 @@ local function set_background(content)
 
     print(content)
 
-	vim.fn.system("feh --bg-max " .. content)
+    vim.fn.system("feh --bg-max " .. content)
 
 end
 
-
-
 local function select_background(prompt_bufnr, map)
 
-	local function set_the_background(close)
+    local function set_the_background(close)
 
-		local content = require("telescope.actions.state").get_selected_entry(prompt_bufnr)
+        local content = require("telescope.actions.state").get_selected_entry(prompt_bufnr)
 
-		set_background(content.cwd .. "/" .. content.value)
+        set_background(content.cwd .. "/" .. content.value)
 
-		if close then
+        if close then
 
-			require("telescope.actions").close(prompt_bufnr)
+            require("telescope.actions").close(prompt_bufnr)
 
-		end
+        end
 
-	end
+    end
 
+    map("i", "<C-p>", function()
 
+        set_the_background()
 
-	map("i", "<C-p>", function()
-
-		set_the_background()
-
-	end)
+    end)
 
 
 
-	map("i", "<CR>", function()
+    map("i", "<CR>", function()
 
-		set_the_background(true)
+        set_the_background(true)
 
-	end)
+    end)
 
 end
 
 local function refactor(prompt_bufnr)
 
-	local content = require("telescope.actions.state").get_selected_entry(prompt_bufnr)
+    local content = require("telescope.actions.state").get_selected_entry(prompt_bufnr)
 
-	require("telescope.actions").close(prompt_bufnr)
+    require("telescope.actions").close(prompt_bufnr)
 
-	require("refactoring").refactor(content.value)
+    require("refactoring").refactor(content.value)
 
 end
 
-
-
 M.refactors = function()
 
-	require("telescope.pickers").new({}, {
+    require("telescope.pickers").new({}, {
 
-		prompt_title = "refactors",
+        prompt_title = "refactors",
 
-		finder = require("telescope.finders").new_table({
+        finder = require("telescope.finders").new_table({
 
-			results = require("refactoring").get_refactors(),
+            results = require("refactoring").get_refactors(),
 
-		}),
+        }),
 
-		sorter = require("telescope.config").values.generic_sorter({}),
+        sorter = require("telescope.config").values.generic_sorter({}),
 
-		attach_mappings = function(_, map)
+        attach_mappings = function(_, map)
 
-			map("i", "<CR>", refactor)
+            map("i", "<CR>", refactor)
 
-			map("n", "<CR>", refactor)
+            map("n", "<CR>", refactor)
 
-			return true
+            return true
 
-		end,
+        end,
 
-	}):find()
+    }):find()
 
 end
 
@@ -194,19 +184,19 @@ end
 
 M.git_branches = function()
 
-	require("telescope.builtin").git_branches({
+    require("telescope.builtin").git_branches({
 
-		attach_mappings = function(_, map)
+        attach_mappings = function(_, map)
 
-			map("i", "<c-d>", actions.git_delete_branch)
+            map("i", "<c-d>", actions.git_delete_branch)
 
-			map("n", "<c-d>", actions.git_delete_branch)
+            map("n", "<c-d>", actions.git_delete_branch)
 
-			return true
+            return true
 
-		end,
+        end,
 
-	})
+    })
 
 end
 
@@ -214,191 +204,190 @@ end
 
 M.dev = function(opts)
 
-	opts = opts or {}
+    opts = opts or {}
 
 
 
-	opts.cwd = opts.cwd or vim.loop.fs_realpath(vim.loop.cwd())
+    opts.cwd = opts.cwd or vim.loop.fs_realpath(vim.loop.cwd())
 
-	print("HEY BAE", opts.cwd)
+    print("HEY BAE", opts.cwd)
 
 
 
-	local possible_files = vim.api.nvim_get_runtime_file("/lua/**/dev.lua", true)
+    local possible_files = vim.api.nvim_get_runtime_file("/lua/**/dev.lua", true)
 
-	local local_files = {}
+    local local_files = {}
 
-	for _, raw_f in ipairs(possible_files) do
+    for _, raw_f in ipairs(possible_files) do
 
-		local real_f = vim.loop.fs_realpath(raw_f)
+        local real_f = vim.loop.fs_realpath(raw_f)
 
 
 
-		if string.find(real_f, opts.cwd, 1, true) then
+        if string.find(real_f, opts.cwd, 1, true) then
 
-			table.insert(local_files, real_f)
+            table.insert(local_files, real_f)
 
-		end
+        end
 
-	end
+    end
 
 
 
-	local dev = local_files[1]
+    local dev = local_files[1]
 
-	local loaded = loadfile(dev)
+    local loaded = loadfile(dev)
 
-	local ok, mod = pcall(loaded)
+    local ok, mod = pcall(loaded)
 
-	if not ok then
+    if not ok then
 
-		print("===================================================")
+        print("===================================================")
 
-		print("HEY PRIME. YOUR CODE DOESNT WORK. THIS IS NOT ON ME")
+        print("HEY PRIME. YOUR CODE DOESNT WORK. THIS IS NOT ON ME")
 
-		print("===================================================")
+        print("===================================================")
 
-		return
+        return
 
-	end
+    end
 
 
 
-	-- P(mod)
+    -- P(mod)
 
-	local objs = {}
+    local objs = {}
 
-	for k, v in pairs(mod) do
+    for k, v in pairs(mod) do
 
-		local debug_info = debug.getinfo(v)
+        local debug_info = debug.getinfo(v)
 
-		table.insert(objs, {
+        table.insert(objs, {
 
-			filename = string.sub(debug_info.source, 2),
+            filename = string.sub(debug_info.source, 2),
 
-			text = k,
+            text = k,
 
-		})
+        })
 
-	end
+    end
 
 
 
-	local mod_name = vim.split(dev, "/lua/")
+    local mod_name = vim.split(dev, "/lua/")
 
-	if #mod_name ~= 2 then
+    if #mod_name ~= 2 then
 
-		print("===================================================")
+        print("===================================================")
 
-		print("HEY PRIME. I DO NOT KNOW HOW TO FIND THIS FILE:")
+        print("HEY PRIME. I DO NOT KNOW HOW TO FIND THIS FILE:")
 
-		print(dev)
+        print(dev)
 
-		print("===================================================")
+        print("===================================================")
 
-	end
+    end
 
-	mod_name = string.gsub(mod_name[2], ".lua$", "")
+    mod_name = string.gsub(mod_name[2], ".lua$", "")
 
-	mod_name = string.gsub(mod_name, "/", ".")
+    mod_name = string.gsub(mod_name, "/", ".")
 
 
 
-	pickers.new({
+    pickers.new({
 
-		finder = finders.new_table({
+        finder = finders.new_table({
 
-			results = objs,
+            results = objs,
 
-			entry_maker = function(entry)
+            entry_maker = function(entry)
 
-				return {
+                return {
 
-					value = entry,
+                    value = entry,
 
-					text = entry.text,
+                    text = entry.text,
 
-					display = entry.text,
+                    display = entry.text,
 
-					ordinal = entry.text,
+                    ordinal = entry.text,
 
-					filename = entry.filename,
+                    filename = entry.filename,
 
-				}
+                }
 
-			end,
+            end,
 
-		}),
+        }),
 
-		sorter = conf.generic_sorter(opts),
+        sorter = conf.generic_sorter(opts),
 
-		previewer = previewers.builtin.new(opts),
+        previewer = previewers.builtin.new(opts),
 
-		attach_mappings = function(_, map)
+        attach_mappings = function(_, map)
 
-			actions.select_default:replace(function(...)
+            actions.select_default:replace(function(...)
 
-				-- print("SELECTED", vim.inspect(action_state.get_selected_entry()))
+                -- print("SELECTED", vim.inspect(action_state.get_selected_entry()))
 
-				local entry = action_state.get_selected_entry()
+                local entry = action_state.get_selected_entry()
 
-				actions.close(...)
+                actions.close(...)
 
 
 
-				mod[entry.value.text]()
+                mod[entry.value.text]()
 
-			end)
+            end)
 
 
 
-			map("i", "<tab>", function(...)
+            map("i", "<tab>", function(...)
 
-				local entry = action_state.get_selected_entry()
+                local entry = action_state.get_selected_entry()
 
-				actions.close(...)
+                actions.close(...)
 
 
 
-				vim.schedule(function()
+                vim.schedule(function()
 
-					-- vim.cmd(string.format([[normal!]], entry.value.text))
+                    -- vim.cmd(string.format([[normal!]], entry.value.text))
 
-					vim.api.nvim_feedkeys(
+                    vim.api.nvim_feedkeys(
 
-						vim.api.nvim_replace_termcodes(
+                        vim.api.nvim_replace_termcodes(
 
-							string.format("<esc>:lua require('%s').%s()", mod_name, entry.value.text),
+                            string.format("<esc>:lua require('%s').%s()", mod_name, entry.value.text),
 
-							true,
+                            true,
 
-							false,
+                            false,
 
-							true
+                            true
 
-						),
+                        ),
 
-						"n",
+                        "n",
 
-						true
+                        true
 
-					)
+                    )
 
-				end)
+                end)
 
-			end)
+            end)
 
 
 
-			return true
+            return true
 
-		end,
+        end,
 
-	}):find()
+    }):find()
 
 end
 
 
 
 return M
-
